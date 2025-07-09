da556cd71317466bf3b20622e44e9e72037827d8
Dennis De Biasi <dennisloiak11@gmail.com>
2025-07-09 17:58:53 +0200

    Commit EsempioPatternFacade e EsercizioFacade

---
+++ src/Mercoledì9Luglio/EsempioPatternFacade.java
@@ -0,0 +1,30 @@
+package Mercoledì9Luglio;
+
+class SubsystemA {
+    void operationA() {
+        System.out.println("Operazione A del sottosistema A");
+    }
+}
+
+class SubsystemB {
+    void operationB() {
+        System.out.println("Operazione B del sottosistema B");
+    }
+}
+
+class Facade {
+    private SubsystemA subsystemA = new SubsystemA();
+    private SubsystemB subsystemB = new SubsystemB();
+
+    public void operazioneUnificata() {
+        subsystemA.operationA();
+        subsystemB.operationB();
+    }
+}
+
+public class EsempioPatternFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.operazioneUnificata();
+    }
+}
