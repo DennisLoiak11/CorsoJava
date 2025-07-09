843a73daa028767c5031dbded4e53efa7950f12d
Dennis De Biasi <dennisloiak11@gmail.com>
2025-07-09 15:43:24 +0200

    Commit esempio utilizzo pattern Strategy ed EsercizioStrategy

---
+++ src/Mercoledì9Luglio/EsempioPatternStrategy.java
@@ -0,0 +1,39 @@
+package Mercoledì9Luglio;
+
+interface Strategy{
+    void execute();
+}
+
+class ConcreteStrategyA implements Strategy{
+    public void execute(){
+        System.out.println("Strategia A eseguita");
+    }
+}
+
+class ConcreteStrategyB implements Strategy{
+    public void execute(){
+        System.out.println("Strategia B eseguita");
+    }
+}
+
+class Context{
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void performTask(){
+        strategy.execute();
+    }
+}
+
+public class EsempioPatternStrategy {
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setStrategy(new ConcreteStrategyA());
+        context.performTask();
+        context.setStrategy(new ConcreteStrategyB());
+        context.performTask();
+    }
+}
